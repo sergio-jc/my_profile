aa50a0f1ad88d0f6ee84899e1283c93d5b4001a0
Your Namesergio-jc <sergiojara0609@gmail.com>
2022-08-06 16:01:00 -0500

    add css Nav

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,11 +1,32 @@
-import menu from '../../images/logos/menu.svg'
-import '../../css/Nav.css'
+import menu from "../../images/logos/menu.svg";
+import "../../css/Nav.css";
 const Nav = () => {
   return (
-    <div className='menu_icon'>
-        <img src={menu} alt="menu_icon"/>
+    <div className="menu_icon">
+      <nav className="nav">
+        <a href="#menu" className="nav__target">
+          <img src={menu} alt="menu_icon" />
+        </a>
+        <div className="nav__links" id="menu">
+          <a href="#sec1" className="nav__link">
+            HOME
+          </a>
+          <a href="#sec2" className="nav__link">
+            ABOUT ME
+          </a>
+          <a href="#sec3" className="nav__link">
+            SKILLSET
+          </a>
+          <a href="#sec4" className="nav__link">
+            PORTFOLIO
+          </a>
+          <a href="#sec5" className="nav__link">
+            TESTIMONIALS
+          </a>
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
