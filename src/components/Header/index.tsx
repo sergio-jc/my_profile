46863b6a74bb49be7ad5ba9a9140ef735bb93efa
Your Namesergio-jc <sergiojara0609@gmail.com>
2022-08-06 10:58:58 -0500

    add media queries aboutme header protfolio

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,14 +5,10 @@ import "../../css/Header.css";
 const Header = () => {
   const [count, setCount] = useState(0);
   return (
-    <>
-      <header className="App-header">
-        <h1>Frontend Developer</h1>
-        <p>
-          I design and code beautifully simple things, and I love what I do.
-        </p>
-      </header>
-    </>
+    <div className="App-header">
+      <h1>Frontend Developer</h1>
+      <p>I design and code beautifully simple things, and I love what I do.</p>
+    </div>
   );
 };
 
