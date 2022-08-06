9393b0574e4fe27a1d665ae749cc43684f8844e0
Your Namesergio-jc <sergiojara0609@gmail.com>
2022-08-05 22:51:00 -0500

    add images/svg hi programming

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 
 import "./App.css";
+import AboutMe from "./components/AboutMe";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <Header/> 
+      <AboutMe/>
     </div>
   );
 }
---
+++ src/components/AboutMe/index.tsx
@@ -0,0 +1,12 @@
+
+import programming from "../../images/svg/programming.svg";
+
+const AboutMe = () => {
+  return (
+    <div>
+        <img src={programming} />
+    </div>
+  )
+}
+
+export default AboutMe
