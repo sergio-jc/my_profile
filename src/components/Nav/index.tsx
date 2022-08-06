fa7296cb6413ab00c2bda5f207daaf28024fea60
Your Namesergio-jc <sergiojara0609@gmail.com>
2022-08-06 16:19:31 -0500

    add X Nav component

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,29 +1,60 @@
 import menu from "../../images/logos/menu.svg";
+import x from "../../images/logos/x.svg";
+
 import "../../css/Nav.css";
+import { useState } from "react";
 const Nav = () => {
+  const [toggle, setToggle] = useState(false);
+  let menuIcon = toggle ? x : menu;
   return (
     <div className="menu_icon">
       <nav className="nav">
         <a href="#menu" className="nav__target">
-          <img src={menu} alt="menu_icon" />
+          <img
+            src={menuIcon}
+            alt="menu_icon"
+            onClick={() => setToggle(!toggle)}
+          />
         </a>
-        <div className="nav__links" id="menu">
-          <a href="#sec1" className="nav__link">
-            HOME
-          </a>
-          <a href="#sec2" className="nav__link">
-            ABOUT ME
-          </a>
-          <a href="#sec3" className="nav__link">
-            SKILLSET
-          </a>
-          <a href="#sec4" className="nav__link">
-            PORTFOLIO
-          </a>
-          <a href="#sec5" className="nav__link">
-            TESTIMONIALS
-          </a>
-        </div>
+        {toggle && (
+          <div className="nav__links" id="menu">
+            <a
+              href="#sec1"
+              className="nav__link"
+              onClick={() => setToggle(!toggle)}
+            >
+              HOME
+            </a>
+            <a
+              href="#sec2"
+              className="nav__link"
+              onClick={() => setToggle(!toggle)}
+            >
+              ABOUT ME
+            </a>
+            <a
+              href="#sec3"
+              className="nav__link"
+              onClick={() => setToggle(!toggle)}
+            >
+              SKILLSET
+            </a>
+            <a
+              href="#sec4"
+              className="nav__link"
+              onClick={() => setToggle(!toggle)}
+            >
+              PORTFOLIO
+            </a>
+            <a
+              href="#sec5"
+              className="nav__link"
+              onClick={() => setToggle(!toggle)}
+            >
+              TESTIMONIALS
+            </a>
+          </div>
+        )}
       </nav>
     </div>
   );
